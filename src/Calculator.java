f6e0ddb5a76789b745002cce71bd1b792dee1b37
Samir Babayev <samir.m.babayev@gmail.com>
2022-05-06 15:10:23 -0400

    1st commit

---
+++ src/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+    // 1st adding
+    public static double add(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public static double subtract(double num1, double num2){
+        double result = num1 - num2;
+        return num1 - num2;
+    }
+    // 2nd adding
+    public static double multiply(double num1, double num2){
+        return num1*num2;
+    }
+        
+    public static double divide(double n1, double n2){
+        double result = n1/n2;
+        return result;
+    }
+   
+}
